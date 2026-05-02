e26917f58f97fc62e528103947d3ca56906c1a7a
CryptoGatsu <tylerlloyd29@gmail.com>
2026-05-02 09:37:57 -0700

    Update projects.js

--- api/projects.js
+++ api/projects.js
@@ -111,3 +111,7 @@ export default async function handler(req, res){
     return res.status(500).json({ error:"server crash" })
   }
 }
+
+console.log("BODY:", req.body)
+console.log("SUPABASE_URL:", SUPABASE_URL)
+console.log("KEY exists:", !!KEY)
